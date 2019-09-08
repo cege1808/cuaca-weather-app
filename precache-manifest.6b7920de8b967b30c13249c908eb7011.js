48e4d5e3bcc5b38b6e74550dc6f51e40d600e451
Clarissa Gunawan <gunawan.clarissa@hotmail.com>
2019-09-07 21:25:31 -0700

    Updates

---
+++ precache-manifest.6b7920de8b967b30c13249c908eb7011.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f8d451cacadd61c3ab34857b4309765f",
+    "url": "/cuaca-weather-app/index.html"
+  },
+  {
+    "revision": "e32c95df57abe397534e",
+    "url": "/cuaca-weather-app/static/css/main.7c538f11.chunk.css"
+  },
+  {
+    "revision": "5f1f70c2e9dea76b3e23",
+    "url": "/cuaca-weather-app/static/js/2.bffab034.chunk.js"
+  },
+  {
+    "revision": "e32c95df57abe397534e",
+    "url": "/cuaca-weather-app/static/js/main.395c1000.chunk.js"
+  },
+  {
+    "revision": "b3d6b4df0cd544d44600",
+    "url": "/cuaca-weather-app/static/js/runtime~main.a28ddbc1.js"
+  },
+  {
+    "revision": "dba0c688b8d5ee09a1e214aebd5d25e4",
+    "url": "/cuaca-weather-app/static/media/Product Sans Bold.dba0c688.ttf"
+  },
+  {
+    "revision": "e88ec18827526928e71407a24937825a",
+    "url": "/cuaca-weather-app/static/media/Product Sans Italic.e88ec188.ttf"
+  },
+  {
+    "revision": "eae9c18cee82a8a1a52e654911f8fe83",
+    "url": "/cuaca-weather-app/static/media/Product Sans Regular.eae9c18c.ttf"
+  }
+]);
